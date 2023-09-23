9994b1b12e25f2106be8cd57efd5ba4cf8d0808f
Daniel Wilhelmsen <dpwilhelmsen@users.noreply.github.com>
2023-09-23 02:01:50 -0700

    Refactor to stop using createClass

--- build/components/DisqusThread.js
+++ build/components/DisqusThread.js
@@ -1,6 +1,12 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = require('react');
 
@@ -13,6 +19,12 @@ var _propTypes2 = _interopRequireDefault(_propTypes);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var DISQUS_CONFIG = ['shortname', 'identifier', 'title', 'url', 'category_id', 'onNewComment', 'api_key', 'remote_auth_s3', 'targetWindow', 'sso'];
 var __disqusAdded = false;
@@ -48,164 +60,185 @@ function isScriptLoaded(url, document) {
   return false;
 }
 
-module.exports = _react2.default.createClass({
-  displayName: 'DisqusThread',
+var DisqusThread = function (_Component) {
+  _inherits(DisqusThread, _Component);
 
-  propTypes: {
-    id: _propTypes2.default.string,
+  function DisqusThread() {
+    _classCallCheck(this, DisqusThread);
 
-    /**
-     * `shortname` tells the Disqus service your forum's shortname,
-     * which is the unique identifier for your website as registered
-     * on Disqus. If undefined , the Disqus embed will not load.
-     */
-    shortname: _propTypes2.default.string.isRequired,
+    return _possibleConstructorReturn(this, (DisqusThread.__proto__ || Object.getPrototypeOf(DisqusThread)).apply(this, arguments));
+  }
 
-    /**
-     * `identifier` tells the Disqus service how to identify the
-     * current page. When the Disqus embed is loaded, the identifier
-     * is used to look up the correct thread. If disqus_identifier
-     * is undefined, the page's URL will be used. The URL can be
-     * unreliable, such as when renaming an article slug or changing
-     * domains, so we recommend using your own unique way of
-     * identifying a thread.
-     */
-    identifier: _propTypes2.default.string,
-
-    /**
-     * `title` tells the Disqus service the title of the current page.
-     * This is used when creating the thread on Disqus for the first time.
-     * If undefined, Disqus will use the <title> attribute of the page.
-     * If that attribute could not be used, Disqus will use the URL of the page.
-     */
-    title: _propTypes2.default.string,
-
-    /**
-     * `url` tells the Disqus service the URL of the current page.
-     * If undefined, Disqus will take the window.location.href.
-     * This URL is used to look up or create a thread if disqus_identifier
-     * is undefined. In addition, this URL is always saved when a thread is
-     * being created so that Disqus knows what page a thread belongs to.
-     */
-    url: _propTypes2.default.string,
-
-    /**
-     * `category_id` tells the Disqus service the category to be used for
-     * the current page. This is used when creating the thread on Disqus
-     * for the first time.
-     */
-    category_id: _propTypes2.default.string,
-
-    /**
-     * `onNewComment` function accepts one parameter `comment` which is a
-     * JavaScript object with comment `id` and `text`. This allows you to track
-     * user comments and replies and run a script after a comment is posted.
-     */
-    onNewComment: _propTypes2.default.func,
-
-    /**
-    * `api_key` tells Disqus service there is a key available for SSO.
-    */
-    api_key: _propTypes2.default.string,
-    /**
-    *
-    * `remote_auth_s3` is needed for Disqus SSO
-    *
-    */
-    remote_auth_s3: _propTypes2.default.string,
-    /**
-     * `sso` is an object with properties for a Disqus sso section
-     *
-     */
-    sso: _propTypes2.default.object
-
-  },
-
-  getDefaultProps: function getDefaultProps() {
-    return {
-      shortname: null,
-      identifier: null,
-      title: null,
-      url: null,
-      category_id: null,
-      onNewComment: null,
-      api_key: null,
-      remote_auth_s3: null,
-      sso: null,
-      targetWindow: null
-    };
-  },
-  componentDidMount: function componentDidMount() {
-    this.loadDisqus();
-  },
-  componentDidUpdate: function componentDidUpdate() {
-    this.loadDisqus();
-  },
-  shouldComponentUpdate: function shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.identifier !== this.props.identifier || nextProps.remote_auth_s3 !== this.props.remote_auth_s3;
-  },
-  render: function render() {
-    var _this = this;
-
-    var props = Object.keys(this.props).reduce(function (memo, key) {
-      return DISQUS_CONFIG.some(function (config) {
-        return config === key;
-      }) ? memo : _extends({}, memo, _defineProperty({}, key, _this.props[key]));
-    }, {});
-    return _react2.default.createElement(
-      'div',
-      props,
-      _react2.default.createElement('div', { id: 'disqus_thread' })
-    );
-  },
-  addDisqusScript: function addDisqusScript() {
-    var targetWindow = this.props.targetWindow || window;
-    if (isScriptLoaded('//' + this.props.shortname + '.disqus.com/embed.js', targetWindow.document)) {
-      return;
+  _createClass(DisqusThread, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      this.loadDisqus();
     }
+  }, {
+    key: 'componentDidUpdate',
+    value: function componentDidUpdate() {
+      this.loadDisqus();
+    }
+  }, {
+    key: 'shouldComponentUpdate',
+    value: function shouldComponentUpdate(nextProps, nextState) {
+      return nextProps.identifier !== this.props.identifier || nextProps.remote_auth_s3 !== this.props.remote_auth_s3;
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var _this2 = this;
 
-    var child = this.disqus = targetWindow.document.createElement('script');
-    var parent = targetWindow.document.getElementsByTagName('head')[0] || targetWindow.document.getElementsByTagName('body')[0];
-
-    child.async = true;
-    child.type = 'text/javascript';
-    child.src = '//' + this.props.shortname + '.disqus.com/embed.js';
-
-    parent.appendChild(child);
-    __disqusAdded = true;
-  },
-  loadDisqus: function loadDisqus() {
-    var _this2 = this;
-
-    var props = {};
-    var targetWindow = this.props.targetWindow || window;
-
-    // Extract Disqus props that were supplied to this component
-    DISQUS_CONFIG.forEach(function (prop) {
-      if (!!_this2.props[prop]) {
-        props[prop] = _this2.props[prop];
+      var props = Object.keys(this.props).reduce(function (memo, key) {
+        return DISQUS_CONFIG.some(function (config) {
+          return config === key;
+        }) ? memo : _extends({}, memo, _defineProperty({}, key, _this2.props[key]));
+      }, {});
+      return _react2.default.createElement(
+        'div',
+        props,
+        _react2.default.createElement('div', { id: 'disqus_thread' })
+      );
+    }
+  }, {
+    key: 'addDisqusScript',
+    value: function addDisqusScript() {
+      var targetWindow = this.props.targetWindow || window;
+      if (isScriptLoaded('//' + this.props.shortname + '.disqus.com/embed.js', targetWindow.document)) {
+        return;
       }
-    });
 
-    // Always set URL
-    if (!props.url || !props.url.length) {
-      props.url = targetWindow.location.href;
+      var child = this.disqus = targetWindow.document.createElement('script');
+      var parent = targetWindow.document.getElementsByTagName('head')[0] || targetWindow.document.getElementsByTagName('body')[0];
+
+      child.async = true;
+      child.type = 'text/javascript';
+      child.src = '//' + this.props.shortname + '.disqus.com/embed.js';
+
+      parent.appendChild(child);
+      __disqusAdded = true;
     }
+  }, {
+    key: 'loadDisqus',
+    value: function loadDisqus() {
+      var _this3 = this;
 
-    // If Disqus has already been added, reset it
-    if (typeof targetWindow.DISQUS !== 'undefined') {
-      targetWindow.DISQUS.reset({
-        reload: true,
-        config: function config() {
-          copyProps(this.page, props);
-          // Disqus needs hashbang URL, see https://help.disqus.com/customer/portal/articles/472107
-          this.page.url = this.page.url.replace(/#/, '') + '#!newthread';
+      var props = {};
+      var targetWindow = this.props.targetWindow || window;
+
+      // Extract Disqus props that were supplied to this component
+      DISQUS_CONFIG.forEach(function (prop) {
+        if (!!_this3.props[prop]) {
+          props[prop] = _this3.props[prop];
         }
       });
-    } else {
-      // Otherwise add Disqus to the page
-      copyProps(targetWindow, props, 'disqus_');
-      this.addDisqusScript();
+
+      // Always set URL
+      if (!props.url || !props.url.length) {
+        props.url = targetWindow.location.href;
+      }
+
+      // If Disqus has already been added, reset it
+      if (typeof targetWindow.DISQUS !== 'undefined') {
+        targetWindow.DISQUS.reset({
+          reload: true,
+          config: function config() {
+            copyProps(this.page, props);
+            // Disqus needs hashbang URL, see https://help.disqus.com/customer/portal/articles/472107
+            this.page.url = this.page.url.replace(/#/, '') + '#!newthread';
+          }
+        });
+      } else {
+        // Otherwise add Disqus to the page
+        copyProps(targetWindow, props, 'disqus_');
+        this.addDisqusScript();
+      }
     }
-  }
-});
+  }]);
+
+  return DisqusThread;
+}(_react.Component);
+
+DisqusThread.propTypes = {
+  id: _propTypes2.default.string,
+
+  /**
+   * `shortname` tells the Disqus service your forum's shortname,
+   * which is the unique identifier for your website as registered
+   * on Disqus. If undefined , the Disqus embed will not load.
+   */
+  shortname: _propTypes2.default.string.isRequired,
+
+  /**
+   * `identifier` tells the Disqus service how to identify the
+   * current page. When the Disqus embed is loaded, the identifier
+   * is used to look up the correct thread. If disqus_identifier
+   * is undefined, the page's URL will be used. The URL can be
+   * unreliable, such as when renaming an article slug or changing
+   * domains, so we recommend using your own unique way of
+   * identifying a thread.
+   */
+  identifier: _propTypes2.default.string,
+
+  /**
+   * `title` tells the Disqus service the title of the current page.
+   * This is used when creating the thread on Disqus for the first time.
+   * If undefined, Disqus will use the <title> attribute of the page.
+   * If that attribute could not be used, Disqus will use the URL of the page.
+   */
+  title: _propTypes2.default.string,
+
+  /**
+   * `url` tells the Disqus service the URL of the current page.
+   * If undefined, Disqus will take the window.location.href.
+   * This URL is used to look up or create a thread if disqus_identifier
+   * is undefined. In addition, this URL is always saved when a thread is
+   * being created so that Disqus knows what page a thread belongs to.
+   */
+  url: _propTypes2.default.string,
+
+  /**
+   * `category_id` tells the Disqus service the category to be used for
+   * the current page. This is used when creating the thread on Disqus
+   * for the first time.
+   */
+  category_id: _propTypes2.default.string,
+
+  /**
+   * `onNewComment` function accepts one parameter `comment` which is a
+   * JavaScript object with comment `id` and `text`. This allows you to track
+   * user comments and replies and run a script after a comment is posted.
+   */
+  onNewComment: _propTypes2.default.func,
+
+  /**
+   * `api_key` tells Disqus service there is a key available for SSO.
+   */
+  api_key: _propTypes2.default.string,
+  /**
+   *
+   * `remote_auth_s3` is needed for Disqus SSO
+   *
+   */
+  remote_auth_s3: _propTypes2.default.string,
+  /**
+   * `sso` is an object with properties for a Disqus sso section
+   *
+   */
+  sso: _propTypes2.default.object
+};
+
+DisqusThread.defaultProps = {
+  shortname: null,
+  identifier: null,
+  title: null,
+  url: null,
+  category_id: null,
+  onNewComment: null,
+  api_key: null,
+  remote_auth_s3: null,
+  sso: null,
+  targetWindow: null
+};
+
+exports.default = DisqusThread;
